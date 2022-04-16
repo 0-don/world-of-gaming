995f3688bc86e54d8f9e352987fc16584cdcbb78
Don-Cryptus <don.cryptus@gmail.com>
2022-04-16 21:26:32 +0200

    remove unused packages

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,13 +1,3 @@
-import produce, {Draft} from 'immer';
-import {State, StateCreator} from 'zustand';
-
-export const immer =
-  <T extends State>(
-    config: StateCreator<T, (fn: (draft: Draft<T>) => void) => void>,
-  ): StateCreator<T> =>
-  (set, get, api) =>
-    config(fn => set(produce<T>(fn)), get, api);
-
 export const colorHexString = (rating: number) => {
   if (rating >= 75) {
     return '#66cc33';
