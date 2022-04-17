18c5aaf5f43a63cf9d6e9e19cb9525f9e84873d3
Don-Cryptus <don.cryptus@gmail.com>
2022-04-17 22:08:27 +0200

    change app name

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,10 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+    bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  plugins: [require('prettier-plugin-tailwindcss')],
 };
